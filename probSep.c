0585bc2943ee0c44143b32955c63128a50390f9c
Jimmy Velasco <jvelas00@g.uafs.edu>
2021-09-01 15:55:08 +0000

    added example

---
+++ probSep.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+/* Function Prototype */
+void loadArray(int array[], int size);
+
+int main(int argc, char *argv[]){
+   int numbers[SIZE];
+
+   loadArray(numbers,SIZE);
+   printf("%d\n",numbers[9]);
+   return 0;
+
+
+}
+
+void loadArray(int array[], int size){
+    int index;
+
+    for(index=0;index<size;index++){
+	array[index] = index * 100;
+
+    }
+}	
